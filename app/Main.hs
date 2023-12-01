774bfcb763a75e2d2f6e89a6ce66b661153e82b3
p6nj <breee@duck.com>
2023-12-01 08:31:28 +0100

    aoc 2023 first challenge data

--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,6 @@ module Main where
 
 import Data.List (stripPrefix)
 
--- last year first challenge
+input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"
 
-input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
-
-wordsWhen :: (Char -> Bool) -> String -> [String]
-wordsWhen p s = case dropWhile p s of
-  "" -> []
-  s' -> w : wordsWhen p s''
-    where
-      (w, s'') = break p s'
-
-organise = wordsWhen (== '\n')
-
-main = print $ organise input
+main = print input
