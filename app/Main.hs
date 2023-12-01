10e619e1e44a742d0bed728f935a13ec82f577ad
p6nj <breee@duck.com>
2023-12-01 23:11:54 +0100

    1 p1

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,7 @@
 module Main where
 
+import Data.Char (digitToInt, isDigit)
 import Data.List (stripPrefix)
-
-input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"
 
 wordsWhen p s = case dropWhile p s of
   "" -> []
@@ -10,4 +9,6 @@ wordsWhen p s = case dropWhile p s of
     where
       (w, s'') = break p s'
 
-main = print $ wordsWhen (== '\n') input
+main = do
+  input <- readFile "app/src/1.txt"
+  print $ sum (map ((\s -> read (head s : last s : "") :: Integer) . filter isDigit) (wordsWhen (== '\n') input))
