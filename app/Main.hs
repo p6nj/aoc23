3ff9d563da91da55717cd95b7e088c6298df6c14
p6nj <breee@duck.com>
2023-12-01 13:40:51 +0100

    wordsWhen

--- app/Main.hs
+++ app/Main.hs
@@ -4,4 +4,10 @@ import Data.List (stripPrefix)
 
 input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"
 
-main = print input
+wordsWhen p s = case dropWhile p s of
+  "" -> []
+  s' -> w : wordsWhen p s''
+    where
+      (w, s'') = break p s'
+
+main = print $ wordsWhen (== '\n') input
